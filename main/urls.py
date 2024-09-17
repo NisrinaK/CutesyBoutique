56f4a6ef3c03ec060421d96decb6ec363ca055f4
NisrinaK <nisrina1272@gmail.com>
2024-09-17 14:31:44 +0700

    Mengerjakan beberapa checklist Tugas3

--- main/urls.py
+++ main/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('products/', views.product_list, name='product_list'),
     path('', views.home, name='home'),
     path('add-product/', views.add_product, name='add_product'),
-    path('products/xml/', views.product_xml, name='product_xml'),
+    path('products/xml/', views.product_xml, name='product_xml'), #XML
     path('products/xml/<int:id>/', views.product_xml_by_id, name='product_xml_by_id'),
-    path('products/json/', views.product_json, name='product_json'),
+    path('products/json/', views.product_json, name='product_json'), #JSON
     path('products/json/<int:id>/', views.product_json_by_id, name='product_json_by_id'),
 ]
