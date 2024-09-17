deaad6352e022d9f6e809fc033c9b2dcb74777fa
NisrinaK <nisrina1272@gmail.com>
2024-09-17 14:24:59 +0700

    Mengerjakan beberapa checklist Tugas3

--- main/urls.py
+++ main/urls.py
@@ -6,5 +6,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('info/', views.show_info, name='show_info'),
     path('products/', views.product_list, name='product_list'),
-    path('', views.home, name='home'), 
+    path('', views.home, name='home'),
+    path('add-product/', views.add_product, name='add_product'),
+    path('products/xml/', views.product_xml, name='product_xml'),
+    path('products/xml/<int:id>/', views.product_xml_by_id, name='product_xml_by_id'),
+    path('products/json/', views.product_json, name='product_json'),
+    path('products/json/<int:id>/', views.product_json_by_id, name='product_json_by_id'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
+from .forms import ProductForm
+from django.http import HttpResponse
+from django.core import serializers
 
 def product_list(request):
     products = Product.objects.all()  # Mengambil semua produk dari database
@@ -10,3 +13,36 @@ def show_info(request):
 
 def home(request):
     return render(request, 'main/home.html')
+
+#Menambah Produk
+def add_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')
+    else:
+        form = ProductForm()
+    return render(request, 'main/add_product.html', {'form': form})
+
+#XML
+def product_xml(request):
+    products = Product.objects.all()
+    data = serializers.serialize('xml', products)
+    return HttpResponse(data, content_type='application/xml')
+
+def product_xml_by_id(request, id):
+    product = Product.objects.filter(id=id)
+    data = serializers.serialize('xml', product)
+    return HttpResponse(data, content_type='application/xml')
+
+#JSON
+def product_json(request):
+    products = Product.objects.all()
+    data = serializers.serialize('json', products)
+    return HttpResponse(data, content_type='application/json')
+
+def product_json_by_id(request, id):
+    product = Product.objects.filter(id=id)
+    data = serializers.serialize('json', product)
+    return HttpResponse(data, content_type='application/json')
