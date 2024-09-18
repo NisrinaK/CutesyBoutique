987c03c30723fa6529718eeeb9077bc498fcd0db
NisrinaK <nisrina1272@gmail.com>
2024-09-18 11:28:59 +0700

    Fix tugas 3 plis

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,7 @@ from main.views import add_product, show_json, show_json_by_id, show_xml, show_x
 from main.views import show_home
 app_name = 'main'
 
-#urls path
+#urls path oke
 urlpatterns = [
     path('', show_home, name='show_home'),
     path('add_product/', add_product, name='add_product'),
