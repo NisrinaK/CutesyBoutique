ea111f6175625f0a1e26b7b35965fa2791ba23ae
NisrinaK <nisrina1272@gmail.com>
2024-09-18 07:18:25 +0700

    benerin xml dan json 2

--- main/models.py
+++ main/models.py
@@ -2,6 +2,7 @@ from django.db import models
 import uuid
 
 class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
     price = models.IntegerField()
     description = models.TextField()
