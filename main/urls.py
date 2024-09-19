3a37b3a5bd7091e98724001aab506021df8a484e
NisrinaK <nisrina1272@gmail.com>
2024-09-19 11:57:58 +0700

    TUGAS 4

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from . import views
 from main.views import add_product, show_json, show_json_by_id, show_xml, show_xml_by_id
 from main.views import show_home
+from main.views import register
+from main.views import login_user
+from main.views import logout_user
 app_name = 'main'
 
 #urls path oke
@@ -13,4 +16,7 @@ urlpatterns = [
     path('json/', show_json, name='show_json'),
     path('xml/<uuid:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<uuid:id>/', show_json_by_id, name='show_json_by_id'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
--- main/views.py
+++ main/views.py
@@ -3,6 +3,16 @@ from .models import Product
 from .forms import ProductForm
 from django.http import HttpResponse
 from django.core import serializers
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 def show_info(request):
     return render(request, 'main/info.html')
@@ -15,14 +25,17 @@ def add_product(request):
     form = ProductForm(request.POST or None)
 
     if form.is_valid() and request.method == "POST":
-        form.save()
+        product = form.save(commit=False)
+        product.user = request.user
+        product.save() #form.save()
         return redirect("main:show_home")
 
     context = {'form': form}
-    return render(request, "add_product.html", context)
+    return render(request, "add_product.html", context) #'main/add_product.html'
 
+@login_required(login_url='/login')
 def show_home(request):
-    products = Product.objects.all()
+    products = Product.objects.filter(user=request.user) #products = Product.objects.all()
     context = {
         'barang' : '2323061456',
         'descripsion': 'Pak Bepe',
@@ -30,9 +43,11 @@ def show_home(request):
         'price': 'PBP E',
         'stock': 'PBP E',
         'products' : products,
+        'last_login': request.COOKIES.get('last_login'),
+        'name': request.user.username,
     }
 
-    return render(request, "home.html", context)
+    return render(request, "home.html", context) #(request, 'main/home.html', context)
 
 # XML
 
@@ -52,3 +67,36 @@ def show_json(request):
 def show_json_by_id(request, id):
     product = Product.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", product), content_type="application/json")
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+   if request.method == 'POST':
+      form = AuthenticationForm(data=request.POST)
+
+      if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_home"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+   else:
+      form = AuthenticationForm(request)
+   context = {'form': form}
+   return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))  # Mengarahkan kembali ke halaman login setelah logout
+    response.delete_cookie('last_login')  # Menghapus cookie last_login
+    return response
